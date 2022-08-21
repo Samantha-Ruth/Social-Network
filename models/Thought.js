101aa0a3bba213a2b6a0bf9ec123848471a60e3f
Samantha-Ruth <sam.haberman@gmail.com>
2022-08-21 08:22:23 -0600

    updated Thought model to include Reactions

--- models/Thought.js
+++ models/Thought.js
@@ -1,33 +1,53 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
+
+const ReactionsSchema = new Schema({
+  // set custom id to avoid confusion with parent comment's _id field
+  reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+  },
+  reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280
+  },
+  username: {
+      type: String,
+      required: true
+  },
+  createdAt: {
+      type: Date,
+      default: Date.now,
+      // getter method to format the timestamp
+      get: (createdAtVal) => dateFormat(createdAtVal)
+  }
+},
+{ 
+  toJSON: {
+  getters: true
+  }
+});
 
 const ThoughtSchema = new Schema({
   thoughtText: {
     type: String,
     required: true,
-    // CHECK IF THESE WORK!
-    min: [1],
-    max: [280]
+    minLength: 1,
+    maxLength: 280
   },
   createdAt: {
     type: Date,
     default: Date.now,
+    // using getter method to formate the timestamp
     get: (createdAtVal) => dateFormat(createdAtVal)
-    // **** Use getter method to formate the timestamp on a query
   },
   username: {
     type: String,
     required: true
   },
-  // *** MAKE SURE THESE WORK!! ****
-  // THIS NEEDS TO BE AN ARRAY OF NESTED DOCUMENTS CREATED
-  // WITH THE REACTIONSCHEMA.
-    reactions: [
-        { 
-    type: Schema.Types.ObjectId,
-    ref: 'Reactions'
-        }
-    ]
+  // ARRAY OF NESTED DOCUMENTS CREATED WITH THE REACTIONSCHEMA.
+    reactions: [ReactionsSchema]
 },
 { 
     toJSON: {
@@ -37,8 +57,7 @@ const ThoughtSchema = new Schema({
     id: false
   });
 
-// CREATE A VIRTUAL CALLED "reactionCount" THAT RETRIEVES
-// THE LENGTH OF THE THOUGHT'S REACTIONS ARRAY FIELD ON QUERY.
+// Virtual that retrieves length of thought reaction's array field.
 ThoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
   });
